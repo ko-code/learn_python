aec0720d0741e38ecfc7639f44d22ca3bfd9936f
Cristian Rojas <cristian.rojas.a@outlook.com>
2023-05-02 23:11:04 -0400

    avances 2 dia

--- Intermediate/00_dates.py
+++ Intermediate/00_dates.py
@@ -26,6 +26,44 @@ print_date(now)
 timestamp = now.timestamp()
 
 
-year_2023 = datetime(2023, 1, 1)
+year_2023 = datetime(2023, 11, 1)
 
 print_date(year_2023)
+
+from datetime import time
+
+current_time = time(21, 6, 55)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+from datetime import date
+
+current_date = date(1995, 2, 28)
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(current_date.year + 1, current_date.month, current_date.day)
+
+print(current_date.year)
+
+
+from datetime import timedelta
+
+diff = year_2023 - now
+print(diff)
+
+diff = year_2023.date() - current_date
+print(diff)
+
+
+
+start_timedelta = timedelta(200, 100, 100, weeks = 10)
+end_timedelta = timedelta(300, 100, 100, weeks = 13)
+
+print(end_timedelta - start_timedelta)
+print(end_timedelta + start_timedelta)
+print(end_timedelta / start_timedelta)
---
+++ Intermediate/01_list_comprehesion.py
@@ -0,0 +1,18 @@
+### List Comprehension ####
+
+
+my_original_list = [35, 40, 50, 70, 21, 29]
+print(my_original_list)
+
+my_list = [i for i in range(7)]
+
+print(my_list)
+
+my_range = range(8)
+print(list(my_range))
+
+my_list = [i + 1 for i in range(7)]
+print(my_list)
+
+my_list = [i * i for i in range(7)]
+print(my_list)
